314b0c66624839711375613855ad4903e081dc9e
Vũ Đức Tiến <vuductien2908@gmail.com>
2022-12-12 12:01:29 +0700

    Complete 85% function customer

---
+++ Customer/App/views/page/orders/cart.php
@@ -0,0 +1,73 @@
+<div class="cart">
+    <div class="container">
+        <div class="cart__container">
+            <div class="cart__title">
+                <h1>Thông tin giỏ hàng</h1>
+            </div>
+            <div class="cart__table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tên sản phẩm</th>
+                            <th lass="w-10">Đơn giá</th>
+                            <th class="w-10">Số lượng</th>
+                            <th lass="w-10">Thành tiền</th>
+                            <th class="w-30">Xóa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="img">
+                                    <img src="../product_img/2.jpg" alt="">
+                                </div>
+                                <p>Giày lười mono</p>
+                            </td>
+                            <td class="w-10 text-right">
+                                555000
+                            </td>
+                            <td class="w-10 text-center">
+                                <input type="number" name="" id="" value="1" min="1">
+                            </td>
+                            <td class="w-10 text-right">550000</td>
+                            <td class="w-30 text-center">
+                                <a href="#">
+                                    <i class="fa-solid fa-xmark"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="img">
+                                    <img src="../product_img/2.jpg" alt="">
+                                </div>
+                                <p>Giày lười mono</p>
+                            </td>
+                            <td class="w-10 text-right">
+                                555000
+                            </td>
+                            <td class="w-10 text-center">
+                                <input type="number" name="" id="" value="1" min="1">
+                            </td>
+                            <td class="w-10 text-right">550000</td>
+                            <td class="w-30 text-center">
+                                <a href="#">
+                                    <i class="fa-solid fa-xmark"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="table__bottom">
+                    <div class="table__total">
+                        <p>Tổng giá sản phẩm</p>
+                        <p>1,000,000</p>
+                    </div>
+                    <div class="table__pay">
+                        <button>Tiến hành thanh toán</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
